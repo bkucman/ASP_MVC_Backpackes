3cf9746b04f7da7a89459ae2cc166873c34448da
kucmeno <kucmeno12@o2.pl>
2018-01-20 18:28:41 +0100

    Add project files.

---
+++ ASP_MVC_Backpackes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_MVC_Backpackes.Startup))]
+namespace ASP_MVC_Backpackes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
